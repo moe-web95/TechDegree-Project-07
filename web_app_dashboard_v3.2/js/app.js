b55d0338d5c66e86d3de18af4138039ca88a1f29
Moe set <moeset89.myanmar@gmail.com>
2022-03-30 12:18:35 +0200

    Updated

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -1,3 +1,27 @@
+/*Alert Bar */
+const alertBanner = document.getElementById('alert');
+//create the html for the banner
+alertBanner.innerHTML = 
+`
+<div class = "alert-banner"> 
+    <p><strong>Alert:</strong> You have unread messages</p>
+    <p class = "alert-banner-close">X</p>
+</div>
+`
+
+alertBanner.addEventListener('click', e =>{
+    const element = e.target;
+    if( element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+    }
+})
+
+
+
+
+
+// Chart Widgets
+
 const trafficCanvas = document.getElementById("traffic-chart");
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
@@ -9,3 +33,28 @@ let trafficData = {
     borderWidth: 1,
     }]
     };
+
+let trafficOptions = {
+    backgroundColor: 'rgba(112, 104, 201, .5)',
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+        display: flase
+        }
+    }
+};
+
+let trafficChart = new Chart (trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+ });
