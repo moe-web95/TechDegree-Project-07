adb36b184a6fa9bc08241ef3487bfca26d4e3747
Moe set <moeset89.myanmar@gmail.com>
2022-03-30 13:11:34 +0200

    UPDATED

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -21,7 +21,7 @@ alertBanner.addEventListener('click', e =>{
 
 
 // Chart Widgets
-
+//Line Graph
 const trafficCanvas = document.getElementById("traffic-chart");
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
@@ -58,3 +58,49 @@ let trafficChart = new Chart (trafficCanvas, {
     data: trafficData,
     options: trafficOptions
  });
+
+ //Bar Graph
+ const dailyCanvas = document.getElementById("daily-chart");
+ //data for daily traffic bar chart,
+ const dailyData = {
+     labels:["S", "M", "T", "W", "T", "F", "S"],
+     datasets: [{
+         label: '# of Hits',
+         data[75, 115, 175, 125, 225, 200, 100],
+         backgroundColor: '#7477BF',
+         borderWidth: 1
+     }]
+ };
+ const dailyOptions = {
+     scales: {
+         y: {
+             beginAtZero: true
+         }
+     },
+     plugins: {
+         legend: {
+         dislay: false
+         }
+     }
+ };
+ let dailyChart = new Chart(dailyCanvas, {
+     type: 'bar',
+     data: dailyData,
+     options: dailyOptions
+ });
+
+ //Doughnut Chart
+ const mobileCanvas = document.getElementById(  "mobile-chart");
+ const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroungColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+ };
